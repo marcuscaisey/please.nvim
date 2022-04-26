f91f3a3bb253bedadc3301664be103248fb30164
mcaisey <mcaisey@thoughtmachine.net>
2022-04-26 15:30:31 +0100

    feat: add padding to popup terminal

--- lua/please/runners.lua
+++ lua/please/runners.lua
@@ -3,6 +3,14 @@ local popup = require 'plenary.popup'
 
 local runners = {}
 
+local close_win = function(winnr)
+  -- If we close multiple windows then sometimes the ones after the first are invalid by the time we get to calling
+  -- nvim_win_close. I'm not sure why this is but telescope.nvim does it as well which is good enough for me.
+  if vim.api.nvim_win_is_valid(winnr) then
+    vim.api.nvim_win_close(winnr, true)
+  end
+end
+
 ---Runs a command with the given args in a terminal in a popup.
 ---The output of the command is automatically scrolled through.
 ---The popup can be exited with q.
@@ -11,16 +19,19 @@ local runners = {}
 runners.popup = function(cmd, args)
   local width = 0.8
   local height = 0.8
-
-  local opts = {
+  local term_win_opts = {
     minwidth = math.ceil(vim.o.columns * width),
     minheight = math.ceil(vim.o.lines * height),
   }
+  local bg_win_oopts = {
+    minwidth = term_win_opts.minwidth + 8,
+    minheight = term_win_opts.minheight + 2,
+  }
 
-  local winnr = popup.create('', opts)
-  local bufnr = vim.fn.winbufnr(winnr)
-
-  local term_chan_id = vim.api.nvim_open_term(bufnr, {})
+  local bg_winnr = popup.create('', bg_win_oopts)
+  local term_winnr = popup.create('', term_win_opts)
+  local term_bufnr = vim.fn.winbufnr(term_winnr)
+  local term_chan_id = vim.api.nvim_open_term(term_bufnr, {})
 
   -- we can still be outputting from the command after it's been shut down, so we need to check this before we send on a
   -- potentially closed channel
@@ -61,12 +72,13 @@ runners.popup = function(cmd, args)
 
   -- disable i -> terminal mode mapping since it's easy to get stuck in terminal mode without any indication that you
   -- need to press <c-\><c-n> to get out
-  vim.keymap.set('n', 'i', '<nop>', { buffer = bufnr })
+  vim.keymap.set('n', 'i', '<nop>', { buffer = term_bufnr })
 
   -- when closing the popup, shutdown the job as well
   local close = function()
     is_shutdown = true
-    vim.api.nvim_win_close(winnr, false)
+    close_win(term_winnr)
+    close_win(bg_winnr)
     -- Calling shutdown in the handler adds a bit of delay before the popup closes for some reason, as if its waiting
     -- for the end of the shutdown call. Maybe it is. Either way, scheduling the shutdown gets rid of the delay.
     vim.schedule(function()
@@ -74,11 +86,11 @@ runners.popup = function(cmd, args)
     end)
   end
   -- close popup on q
-  vim.keymap.set('n', 'q', close, { buffer = bufnr })
+  vim.keymap.set('n', 'q', close, { buffer = term_bufnr })
   -- close popup when focus lost
   vim.api.nvim_create_autocmd({ 'WinLeave' }, {
     group = vim.api.nvim_create_augroup('please.nvim', { clear = true }),
-    buffer = bufnr,
+    buffer = term_bufnr,
     desc = 'close the popup when focus is lost',
     callback = close,
     once = true,
