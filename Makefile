marcuscaisey/please.nvim

commit c072ed045ad7c0afcd64f5b4b14b63677cb52d87

feat: add autocomplete for Please command

TODO
@@ -1,5 +1,4 @@
 - add LSP setup
 - document please filetype detection
-- set up autocomplete for commands
 - document treesitter setup dependency and setup
 - use something else for docs generation (there's some other CLI we can use instead of the docker thing)

lua/please/command.lua
@@ -1,0 +1,86 @@
+-- Exports a single function which creates the Please user command.
+-- complete and user_command return closures since we need to depend on the commands list exported by please.plugin,
+-- however that module imports this one, creating a cycle. We therefore take in a list of commands and their options as
+-- parameters to avoid importing them.
+
+local logging = require 'please.logging'
+
+local M = {}
+
+-- return all args from available_args which:
+-- - start with arg_lead
+-- - are not contained in exclude
+local complete_arg = function(arg_lead, available_args, exclude)
+  exclude = exclude or {}
+  return vim.tbl_filter(function(arg)
+    return vim.startswith(arg, arg_lead) and not vim.tbl_contains(exclude, arg)
+  end, available_args)
+end
+
+-- complete args to the Please command
+local complete = function(cmd_names, cmd_name_to_opts)
+  -- sort all of the commands and opts here so that we only have to do it once (assumes that complete_arg doesn't mess
+  -- with the order)
+  cmd_names = vim.fn.sort(cmd_names)
+  for cmd_name, opts in pairs(cmd_name_to_opts) do
+    cmd_name_to_opts[cmd_name] = vim.fn.sort(opts)
+  end
+
+  -- See :help :command-completion-customlist for detailed description of this func. arg_lead is the arg that we're
+  -- currently completing and cmd_line is a string of the whole current command line. i.e. if we've typed
+  -- 'Please test un', then arg_lead='un' and cmd_line='Please test un'.
+  return function(arg_lead, cmd_line)
+    local cmd_line_words = vim.split(cmd_line, ' ')
+    -- If there's only two words in the command line, then we must be completing the command name. i.e. if cmd_line is
+    -- like 'Please te'
+    local completing_cmd_name = #cmd_line_words == 2
+
+    if completing_cmd_name then
+      return complete_arg(arg_lead, cmd_names)
+    else
+      -- cmd_line looks like 'Please test ...'
+      local cmd_name = cmd_line_words[2]
+      local cmd_opts = cmd_name_to_opts[cmd_name]
+      if cmd_opts then
+        -- We want to exclude the args which already exist in cmd_line. We don't include the last cmd_line word in this
+        -- list since that's the one that we're currently completing.
+        local exclude = { unpack(cmd_line_words, 2, #cmd_line_words - 1) }
+        return complete_arg(arg_lead, cmd_opts, exclude)
+      end
+    end
+  end
+end
+
+-- run a please.nvim command
+local user_command = function(cmds)
+  -- See :help nvim_create_user_command for detailed description of opts. We only use fargs anyway which is just the
+  -- list of args passed to the command.
+  return function(opts)
+    local cmd_name = opts.fargs[1]
+
+    local cmd = cmds[cmd_name]
+    if not cmd then
+      logging.error("'%s' is not a Please command", cmd)
+      return
+    end
+
+    local args = { unpack(opts.fargs, 2) } -- first farg is the command name so ignore that
+    local cmd_opts = {}
+    for _, arg in ipairs(args) do
+      cmd_opts[arg] = true
+    end
+    cmd(cmd_opts)
+  end
+end
+
+M.create_user_command = function(cmds, cmd_name_to_opts)
+  local cmd_names = vim.tbl_keys(cmds)
+  vim.api.nvim_create_user_command('Please', user_command(cmds), {
+    nargs = '+',
+    complete = complete(cmd_names, cmd_name_to_opts),
+    force = true, -- allows us to recreate the command on plugin reload
+    desc = 'Run a please.nvim command.',
+  })
+end
+
+return M

lua/please/init.lua
@@ -75,31 +75,4 @@
 ---
 ---@brief ]]
 
-local please = require 'please.please'
-local logging = require 'please.logging'
-
-vim.g.do_filetype_lua = 1 -- enable Lua filetype detection
-vim.filetype.add {
-  extension = {
-    build_defs = 'please',
-    build_def = 'please',
-    build = 'please',
-    plz = 'please',
-  },
-  filename = {
-    ['BUILD'] = 'please',
-  },
-  pattern = {
-    ['%.plzconfig.*'] = 'dosini',
-  },
-}
-
-return {
-  jump_to_target = please.jump_to_target,
-  build = please.build,
-  test = please.test,
-  run = please.run,
-  yank = please.yank,
-  reload = please.reload,
-  toggle_debug_logs = logging.toggle_debug,
-}
+return require('please.plugin').commands

lua/please/please.lua
@@ -219,14 +219,4 @@ please.yank = function()
   end)
 end
 
----Reload the plugin (for use in development).
-please.reload = function()
-  for pkg, _ in pairs(package.loaded) do
-    if vim.startswith(pkg, 'please') then
-      package.loaded[pkg] = nil
-    end
-  end
-  logging.info 'reloaded plugin'
-end
-
 return please

lua/please/plugin.lua
@@ -1,0 +1,59 @@
+local command = require 'please.command'
+local logging = require 'please.logging'
+local please = require 'please.please'
+
+local M = {}
+
+-- configure all of the file names / extensions which should correspond to the please filetype
+local configure_filetype = function()
+  vim.g.do_filetype_lua = 1 -- enable Lua filetype detection
+  vim.filetype.add {
+    extension = {
+      build_defs = 'please',
+      build_def = 'please',
+      build = 'please',
+      plz = 'please',
+    },
+    filename = {
+      ['BUILD'] = 'please',
+    },
+    pattern = {
+      ['%.plzconfig.*'] = 'dosini',
+    },
+  }
+end
+
+-- create the Please user command
+local create_user_command = function()
+  local cmd_name_to_opts = {
+    test = { 'under_cursor' },
+  }
+  command.create_user_command(M.commands, cmd_name_to_opts)
+end
+
+M.load = function()
+  configure_filetype()
+  create_user_command()
+end
+
+local reload = function()
+  for pkg, _ in pairs(package.loaded) do
+    if vim.startswith(pkg, 'please') then
+      package.loaded[pkg] = nil
+    end
+  end
+  require('please.plugin').load()
+  logging.info 'reloaded plugin'
+end
+
+M.commands = {
+  jump_to_target = please.jump_to_target,
+  build = please.build,
+  test = please.test,
+  run = please.run,
+  yank = please.yank,
+  reload = reload,
+  toggle_debug_logs = logging.toggle_debug,
+}
+
+return M

plugin/please.lua
@@ -9,26 +9,4 @@ end
 
 vim.g.loaded_please = true
 
-local please = require 'please'
-local logging = require 'please.logging'
-
-vim.api.nvim_create_user_command('Please', function(opts)
-  local cmd_name = opts.fargs[1]
-
-  local cmd = please[cmd_name]
-  if not cmd then
-    logging.error("'%s' is not a Please command", cmd)
-    return
-  end
-
-  local args = { unpack(opts.fargs, 2) }
-  local cmd_opts = {}
-  for _, arg in ipairs(args) do
-    cmd_opts[arg] = true
-  end
-  cmd(cmd_opts)
-end, {
-  nargs = '+',
-  force = true,
-  desc = 'Run a please.nvim command.',
-})
+require('please.plugin').load()

commit d0b94a838b5f401587863f7a87238ce5d7415313

fix: add minimal init.vim for tests (broken by c072ed0)

Makefile
@@ -1,7 +1,7 @@
 PWD=$(shell pwd)
 
 test:
-	nvim --headless -c "PlenaryBustedDirectory lua/please/tests {minimal = true}"
+	nvim --headless -c "PlenaryBustedDirectory lua/please/tests { minimal_init = 'lua/please/tests/init.vim' }"
 
 docs:
 	docker build gendocs -t gendocs && \

TODO
@@ -3,3 +3,4 @@
 - document treesitter setup dependency and setup
 - use something else for docs generation (there's some other CLI we can use instead of the docker thing)
 - add demo gifs to README
+- run tests and gendocs in github actions

lua/please/tests/init.vim
@@ -1,0 +1,2 @@
+set rtp+=.
+runtime plugin/please.lua