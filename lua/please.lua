67f499ee20bf3853e8edfd68220445c35dd208b1
Marcus Caisey <marcus@teckna.com>
2023-01-11 18:44:52 +0000

    doc(help): highlight example mappings as lua

--- lua/please.lua
+++ lua/please.lua
@@ -63,18 +63,18 @@
 ---please.nvim doesn't come with any mappings defined out of the box so that you
 ---can customise how you use it. Below are a set of mappings for each available
 ---command to get you started.
---->
+--->lua
 ---  vim.keymap.set('n', '<leader>pj', require('please').jump_to_target)
 ---  vim.keymap.set('n', '<leader>pb', require('please').build)
 ---  vim.keymap.set('n', '<leader>pt', require('please').test)
 ---  vim.keymap.set('n', '<leader>pct', function()
----    require('please').test({ under_cursor = true})
+---    require('please').test({ under_cursor = true })
 ---  end)
 ---  vim.keymap.set('n', '<leader>plt', function()
----    require('please').test({ list = true})
+---    require('please').test({ list = true })
 ---  end)
 ---  vim.keymap.set('n', '<leader>pft', function()
----    require('please').test({ failed = true})
+---    require('please').test({ failed = true })
 ---  end)
 ---  vim.keymap.set('n', '<leader>pr', require('please').run)
 ---  vim.keymap.set('n', '<leader>py', require('please').yank)
