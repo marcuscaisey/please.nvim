a1dedca7a2f9e9ab8f7fed23c950b8195e60ee01
Marcus Caisey <marcus@teckna.com>
2023-03-04 14:41:14 +0000

    test(parsing): rework injection query for highlighting code in tests

--- queries/lua/injections.scm
+++ queries/lua/injections.scm
@@ -1,23 +1,27 @@
-;; extends
+; extends
 
-(field
-  name: (identifier) @_field_name
-  (#eq? @_field_name "tree")
-  value: (table_constructor
+;; comment '--$lang' after a string to highlight the string as $lang
+;; Examples:
+;;   local go = 'x := 2' --go
+;;   go = 'x := 3' --go
+;;   local t = {
+;;     go = 'x := 2' --go
+;;   }
+(
+  [
     (field
-      name: (string
-        content: ("string_content") @_filename)
-        (#lua-match? @_filename "^.+%.go$")
-      value: (string
-        content: ("string_content") @go))))
-
-(field
-  name: (identifier) @_field_name
-  (#eq? @_field_name "tree")
-  value: (table_constructor
-    (field
-      name: (string
-        content: ("string_content") @_filename)
-        (#lua-match? @_filename "^.+%.py")
-      value: (string
-        content: ("string_content") @python))))
+      value: (string ("string_content") @content))
+    (variable_declaration
+      (assignment_statement
+        (expression_list
+          value: (string
+            content: ("string_content") @content))))
+    (assignment_statement
+      (expression_list
+        value: (string
+          content: ("string_content") @content)))
+  ]
+  .
+  (comment
+    content: ("comment_content") @language (#offset! @language 0 1 0 0))
+)
