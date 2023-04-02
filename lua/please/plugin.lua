caaf9d875ed11a67f8e94407dc04aa6635d43dc2
Marcus Caisey <marcus@teckna.com>
2023-04-02 15:26:53 +0100

    fix(plugin): use vim.treesitter.language.register instead of deprecated nvim-treesitter.parsers.filetype_to_parser.name

--- lua/please/plugin.lua
+++ lua/please/plugin.lua
@@ -49,7 +49,12 @@ end
 -- make sure that the python parser is installed and configure it be used for please files
 local configure_treesitter = function()
   require('nvim-treesitter.install').ensure_installed({ 'python' })
-  require('nvim-treesitter.parsers').filetype_to_parsername.please = 'python'
+  -- This method is only available in nvim 0.9+
+  if vim.treesitter.language.register then
+    vim.treesitter.language.register('python', 'please')
+  else
+    require('nvim-treesitter.parsers').filetype_to_parsername.please = 'python'
+  end
 end
 
 M.load = function()
