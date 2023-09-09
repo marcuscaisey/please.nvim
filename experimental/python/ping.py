6019ef49d86de8bb2025392302b9bcef150a863c
Marcus Caisey <marcus@teckna.com>
2023-09-09 13:43:56 +0200

    chore(experimental): use sys.exit in ping.py

--- experimental/python/ping.py
+++ experimental/python/ping.py
@@ -1,4 +1,3 @@
-import os
 import sys
 
 from third_party.python import requests
@@ -6,7 +5,7 @@ from third_party.python import requests
 if __name__ == "__main__":
     if len(sys.argv) < 2:
         print("Provide a URL to ping")
-        os.exit(1)
+        sys.exit(1)
     url = sys.argv[1]
     resp = requests.get(url)
     status = "up" if resp.ok else "down"
