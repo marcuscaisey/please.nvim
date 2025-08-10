285de6909a1977d21c1e8629b5db4534a6f03b8b
Marcus Caisey <marcus@teckna.com>
2025-08-10 18:08:49 +0100

    build: increase minimum nvim version to 0.11

--- plugin/please.lua
+++ plugin/please.lua
@@ -1,4 +1,4 @@
-local min_nvim_version = '0.10.0'
+local min_nvim_version = '0.11.0'
 if vim.fn.has(string.format('nvim-%s', min_nvim_version)) == 0 then
   vim.notify(string.format('please.nvim requires at least Neovim %s', min_nvim_version), vim.log.levels.ERROR)
   return
