eb7dfaa5206393388c6df30de4814ebfaba7bc57
Marcus Caisey <marcus@teckna.com>
2025-08-17 10:55:14 +0100

    refactor(plugin): move most of plugin.lua into plugin/please.lua

--- lua/please/plugin.lua
+++ lua/please/plugin.lua
@@ -1,57 +1,6 @@
-local please = require('please')
-local command = require('please.command')
 local logging = require('please.logging')
-local popup = require('please.runners.popup')
 
 local M = {}
-
--- configure all of the file names / extensions which should correspond to the please filetype
-local function configure_filetype()
-  vim.filetype.add({
-    extension = {
-      build_defs = 'please',
-      build = 'please',
-    },
-    filename = {
-      BUILD = function(path)
-        if vim.fs.root(path, '.plzconfig') then
-          return 'please'
-        end
-        return 'bzl'
-      end,
-      ['BUILD.plz'] = 'please',
-    },
-    pattern = {
-      ['%.plzconfig.*'] = 'dosini',
-    },
-  })
-end
-
--- create the Please user command
-local function create_user_command()
-  local cmds = {
-    restore_popup = popup.restore,
-    toggle_debug_logs = logging.toggle_debug,
-    reload = M.reload,
-  }
-  for k, v in pairs(please) do
-    cmds[k] = v
-  end
-  local cmd_name_to_opts = {
-    test = { 'under_cursor' },
-    debug = { 'under_cursor' },
-  }
-  local cmd_name_to_positional_args = {
-    command = true,
-  }
-
-  command.create_user_command(cmds, cmd_name_to_opts, cmd_name_to_positional_args)
-end
-
-function M.load()
-  configure_filetype()
-  create_user_command()
-end
 
 function M.reload()
   for pkg, _ in pairs(package.loaded) do
--- plugin/please.lua
+++ plugin/please.lua
@@ -8,6 +8,56 @@ if vim.g.loaded_please then
   return
 end
 
-vim.g.loaded_please = true
+local please = require('please')
+local plugin = require('please.plugin')
+local command = require('please.command')
+local logging = require('please.logging')
+local popup = require('please.runners.popup')
 
-require('please.plugin').load()
+-- configure all of the file names / extensions which should correspond to the please filetype
+local function configure_filetype()
+  vim.filetype.add({
+    extension = {
+      build_defs = 'please',
+      build = 'please',
+    },
+    filename = {
+      BUILD = function(path)
+        if vim.fs.root(path, '.plzconfig') then
+          return 'please'
+        end
+        return 'bzl'
+      end,
+      ['BUILD.plz'] = 'please',
+    },
+    pattern = {
+      ['%.plzconfig.*'] = 'dosini',
+    },
+  })
+end
+
+-- create the Please user command
+local function create_user_command()
+  local cmds = {
+    restore_popup = popup.restore,
+    toggle_debug_logs = logging.toggle_debug,
+    reload = plugin.reload,
+  }
+  for k, v in pairs(please) do
+    cmds[k] = v
+  end
+  local cmd_name_to_opts = {
+    test = { 'under_cursor' },
+    debug = { 'under_cursor' },
+  }
+  local cmd_name_to_positional_args = {
+    command = true,
+  }
+
+  command.create_user_command(cmds, cmd_name_to_opts, cmd_name_to_positional_args)
+end
+
+configure_filetype()
+create_user_command()
+
+vim.g.loaded_please = true
