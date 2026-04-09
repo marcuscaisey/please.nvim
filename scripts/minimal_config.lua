e011f542021ca534189ba7250482f88c721c0107
Marcus Caisey <marcus@teckna.com>
2026-04-09 17:28:22 +0100

    chore: minimise minimal config even more

--- scripts/minimal_config.lua
+++ scripts/minimal_config.lua
@@ -1,20 +1,7 @@
 vim.g.mapleader = ' '
 
 -- please.nvim
-vim.cmd('set runtimepath^=.')
-vim.keymap.set('n', '<leader>pb', '<Cmd>Please build<CR>')
-vim.keymap.set('n', '<leader>pr', '<Cmd>Please run<CR>')
-vim.keymap.set('n', '<leader>pt', '<Cmd>Please test<CR>')
-vim.keymap.set('n', '<leader>pct', '<Cmd>Please test under_cursor<CR>')
-vim.keymap.set('n', '<leader>pd', '<Cmd>Please debug<CR>')
-vim.keymap.set('n', '<leader>pcd', '<Cmd>Please debug under_cursor<CR>')
-vim.keymap.set('n', '<leader>ph', '<Cmd>Please history<CR>')
-vim.keymap.set('n', '<leader>pch', '<Cmd>Please clear_history<CR>')
-vim.keymap.set('n', '<leader>pp', '<Cmd>Please set_profile<CR>')
-vim.keymap.set('n', '<leader>pm', '<Cmd>Please maximise_popup<CR>')
-vim.keymap.set('n', '<leader>pj', '<Cmd>Please jump_to_target<CR>')
-vim.keymap.set('n', '<leader>pl', '<Cmd>Please look_up_target<CR>')
-vim.keymap.set('n', '<leader>py', '<Cmd>Please yank<CR>')
+vim.pack.add({ 'https://github.com/marcuscaisey/please.nvim' })
 
 -- LSP
 vim.pack.add({ 'https://github.com/neovim/nvim-lspconfig' })
