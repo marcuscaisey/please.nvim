8670c63fe5b4a3df0c2a465b2d1c93278c2af84c
Marcus Caisey <marcus@teckna.com>
2026-04-16 17:10:13 +0100

    wip: luassert_stubs.lua

---
+++ third_party/lua/library/luassert_stubs.lua
@@ -0,0 +1,21 @@
+---@meta
+
+---Assert that `value == true`.
+---@param value any The value to confirm is `true`.
+---@param msg string? An optional message to display if the assertion fails.
+function assert.is_true(value, msg) end
+
+---Assert that `value == false`.
+---@param value any The value to confirm is `false`.
+---@param msg string? An optional message to display if the assertion fails.
+function assert.is_false(value, msg) end
+
+---Assert that `type(value) == "nil"`.
+---@param value any The value to confirm is of type `nil`.
+---@param msg string? An optional message to display if the assertion fails.
+function assert.is_nil(value, msg) end
+
+---Assert that `type(value) ~= "nil"`.
+---@param value any The value to confirm is not of type `nil`.
+---@param msg string? An optional message to display if the assertion fails.
+function assert.is_not_nil(value, msg) end
