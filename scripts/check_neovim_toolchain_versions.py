57432296dc63131473e1c6e8f7dbcf0eab70adee
Marcus Caisey <marcus@teckna.com>
2026-04-21 23:15:18 +0100

    refactor: wrap check_neovim_toolchain_versions.py in python_binary

--- scripts/check_neovim_toolchain_versions.py
+++ scripts/check_neovim_toolchain_versions.py
@@ -1,8 +1,87 @@
 import collections
-import json
 import re
 import subprocess
 import sys
+from typing import Any
+
+import requests
+
+
+def main() -> None:
+    expected_versions = latest_stable_neovim_versions()
+    actual_versions = repo_neovim_toolchain_versions()
+    expected_strs = (
+        f"{major}.{minor}.{patch}" for major, minor, patch in expected_versions
+    )
+    print(f"Plugin should be tested against Neovim versions {', '.join(expected_strs)}")
+    actual_strs = (
+        f"{major}.{minor}.{patch}" for major, minor, patch in actual_versions
+    )
+    print(f"Repo defines Neovim toolchain versions {', '.join(actual_strs)}")
+    if actual_versions == expected_versions:
+        print("\x1b[1;32mNeovim toolchain versions in repo are up to date\x1b[0m")
+    else:
+        print("\x1b[1;31mNeovim toolchain versions in repo need to be updated\x1b[0m")
+        sys.exit(1)
+
+
+def latest_stable_neovim_versions() -> list[str]:
+    releases = list_releases()
+    latest_patches_by_major_minor = collections.defaultdict(int)
+    for release in releases:
+        if release["draft"] or release["prerelease"]:
+            continue
+        tag_name = release["tag_name"]
+        if not (match := re.fullmatch(r"v(\d+)\.(\d+)\.(\d+)", tag_name)):
+            continue
+        major, minor, patch = (int(n) for n in match.groups())
+        latest_patch = latest_patches_by_major_minor[(major, minor)]
+        latest_patches_by_major_minor[(major, minor)] = max(patch, latest_patch)
+    latest_versions = []
+    for major_minor, latest_patch in sorted(latest_patches_by_major_minor.items())[-2:]:
+        latest_versions.append((*major_minor, latest_patch))
+    if (min_expected_version := latest_versions[0])[2] != 0:
+        min_supported_version = (min_expected_version[0], min_expected_version[1], 0)
+        latest_versions.insert(0, min_supported_version)
+    return latest_versions
+
+
+def list_releases() -> list[Any]:
+    releases = []
+    next_page_url = "https://api.github.com/repos/neovim/neovim/releases?page_size=100"
+    while True:
+        next_page = requests.get(
+            next_page_url,
+            headers={
+                "Accept": "application/vnd.github+json",
+                "X-GitHub-Api-Version": "2026-03-10",
+            },
+        )
+        next_page.raise_for_status()
+
+        releases.extend(next_page.json())
+
+        link_header = next_page.headers.get("link", "")
+        if not (match := re.match(r'(?i)(?<=<)([\S]*)(?=>; rel="next")', link_header)):
+            break
+        next_page_url = match.group(1)
+    return releases
+
+
+def repo_neovim_toolchain_versions() -> list[str]:
+    targets_result = run(
+        "./pleasew", "query", "filter", "--include", "neovim_toolchain:*"
+    )
+    targets = targets_result.splitlines()
+    repo_versions = []
+    for target in targets:
+        version = run(
+            "./pleasew", "query", "print", target, "--label", "neovim_toolchain:"
+        )
+        major, minor, patch = (int(n) for n in version.split("."))
+        repo_versions.append((major, minor, patch))
+    repo_versions.sort()
+    return repo_versions
 
 
 def run(*args):
@@ -15,44 +94,5 @@ def run(*args):
     return result.stdout.strip()
 
 
-releases_json = run("gh", "api", "/repos/neovim/neovim/releases", "--paginate")
-releases = json.loads(releases_json)
-latest_patches_by_major_minor = collections.defaultdict(int)
-for release in releases:
-    if release["draft"] or release["prerelease"]:
-        continue
-    tag_name = release["tag_name"]
-    if not (match := re.fullmatch(r"v(\d+)\.(\d+)\.(\d+)", tag_name)):
-        continue
-    major, minor, patch = (int(n) for n in match.groups())
-    latest_patch = latest_patches_by_major_minor[(major, minor)]
-    latest_patches_by_major_minor[(major, minor)] = max(patch, latest_patch)
-expected_versions = []
-for major_minor, latest_patch in sorted(latest_patches_by_major_minor.items())[-2:]:
-    expected_versions.append((*major_minor, latest_patch))
-if (min_expected_version := expected_versions[0])[2] != 0:
-    min_supported_version = (min_expected_version[0], min_expected_version[1], 0)
-    expected_versions.insert(0, min_supported_version)
-
-targets_result = run("./pleasew", "query", "filter", "--include", "neovim_toolchain:*")
-targets = targets_result.splitlines()
-actual_versions = []
-for target in targets:
-    version = run("./pleasew", "query", "print", target, "--label", "neovim_toolchain:")
-    major, minor, patch = (int(n) for n in version.split("."))
-    actual_versions.append((major, minor, patch))
-actual_versions.sort()
-
-expected_strs = (
-    f"{major}.{minor}.{patch}" for major, minor, patch in expected_versions
-)
-print(f"Plugin should be tested against Neovim versions {', '.join(expected_strs)}")
-actual_strs = (
-    f"{major}.{minor}.{patch}" for major, minor, patch in actual_versions
-)
-print(f"Repo defines Neovim toolchain versions {', '.join(actual_strs)}")
-if actual_versions == expected_versions:
-    print("\x1b[1;32mNeovim toolchain versions in repo are up to date\x1b[0m")
-else:
-    print("\x1b[1;31mNeovim toolchain versions in repo need to be updated\x1b[0m")
-    sys.exit(1)
+if __name__ == "__main__":
+    main()
