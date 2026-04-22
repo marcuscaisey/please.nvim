1852c8717fc469b80df18bab175056e1b9a19f09
Marcus Caisey <marcus@teckna.com>
2026-04-22 12:34:18 +0100

    chore: add mappings to minimal_init.lua

--- scripts/minimal_init.lua
+++ scripts/minimal_init.lua
@@ -1,5 +1,5 @@
 vim.g.loaded_python3_provider = 0
-
+vim.g.mapleader = ' '
 vim.o.signcolumn = 'yes'
 
 -- please.nvim
@@ -14,6 +14,19 @@ please.setup({
     -- configure_pyright = true,
     puku_command = { 'plz', 'puku' },
 })
+vim.keymap.set('n', '<leader>pb', '<Cmd>Please build<CR>')
+vim.keymap.set('n', '<leader>pr', '<Cmd>Please run<CR>')
+vim.keymap.set('n', '<leader>pt', '<Cmd>Please test<CR>')
+vim.keymap.set('n', '<leader>pct', '<Cmd>Please test under_cursor<CR>')
+vim.keymap.set('n', '<leader>pd', '<Cmd>Please debug<CR>')
+vim.keymap.set('n', '<leader>pcd', '<Cmd>Please debug under_cursor<CR>')
+vim.keymap.set('n', '<leader>ph', '<Cmd>Please history<CR>')
+vim.keymap.set('n', '<leader>pch', '<Cmd>Please clear_history<CR>')
+vim.keymap.set('n', '<leader>pp', '<Cmd>Please set_profile<CR>')
+vim.keymap.set('n', '<leader>pm', '<Cmd>Please maximise_popup<CR>')
+vim.keymap.set('n', '<leader>pj', '<Cmd>Please jump_to_target<CR>')
+vim.keymap.set('n', '<leader>pl', '<Cmd>Please look_up_target<CR>')
+vim.keymap.set('n', '<leader>py', '<Cmd>Please yank<CR>')
 
 -- LSP
 vim.pack.add({ 'https://github.com/neovim/nvim-lspconfig', 'https://github.com/mason-org/mason.nvim' })
